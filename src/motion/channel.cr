f1e67ea7819efa9fefe9d451a9abd791d48894ef
Andrew <49383564+andrewc910@users.noreply.github.com>
2020-09-08 17:25:45 -0400

    Change connection handling to allow for more than 1 topic at a time (#37)
    * Fix second channel overwriting first
    
    * Fixed broken channel specs
    
    * Fixed JSON::Serializable not adding state back in
    
    * Fix timers not rendering & added pending spec to test it
    
    * Fix Ameba & add unused_exposures comment

--- src/motion/channel.cr
+++ src/motion/channel.cr
@@ -8,39 +8,42 @@ end
 module Motion
   # :nodoc:
   class Channel < Amber::WebSockets::Channel
-    getter component_connection : Motion::ComponentConnection?
-    property topic : String?
+    property component_connections : Hash(String, Motion::ComponentConnection?) = Hash(String, Motion::ComponentConnection?).new
+    # getter component_connection : Motion::ComponentConnection?
+    # property topic : String?
     property fibers : Array(Fiber) = [] of Fiber
 
     def handle_joined(client_socket, message)
-      state = message["identifier"]["state"].to_s
       client_version = message["identifier"]["version"].to_s
-
       raise_version_mismatch(client_version) if versions_mismatch?(client_version)
 
-      @component_connection = connect_component(state)
-      self.topic = message["topic"].to_s
-      process_periodic_timer
-      synchronize
+      state = message["identifier"]["state"].to_s
+      topic = message["topic"].to_s
+      self.component_connections[topic] = connect_component(state)
+
+      process_periodic_timer(topic)
+      synchronize(topic)
     end
 
-    def handle_leave(client_socket)
+    def handle_leave(client_socket, message)
+      pp "HANDLE LEAVE"
+      pp message
       # TODO: Remove not_nil
-      component_connection.not_nil!.close
-      @component_connection = nil
+      # component_connection.not_nil!.close
+      # @component_connection = nil
     end
 
     def handle_message(client_socket, message)
-      topic = message["topic"]
+      topic = message["topic"].as_s
       identifier, data, command = parse_motion(message["payload"])
 
       case command
       when "unsubscribe"
-        handle_leave(client_socket)
+        handle_leave(client_socket, message)
         broadcast = false
       when "process_motion"
         if data
-          process_motion(identifier, data)
+          process_motion(identifier, data, topic)
           broadcast = true
         end
       end
@@ -48,10 +51,10 @@ module Motion
       synchronize(topic, broadcast)
     end
 
-    def process_motion(identifier, data : JSON::Any)
+    def process_motion(identifier, data : JSON::Any, topic : String)
       motion, raw_event = data["name"], data["event"]
 
-      if (cc = component_connection)
+      if (cc = component_connections[topic])
         cc.process_motion(motion.to_s, Motion::Event.new(raw_event))
       else
         raise "NoComponentConnectionError"
@@ -79,11 +82,11 @@ module Motion
 
       if broadcast
         proc = ->(component : Motion::Base) {
-          render(component)
+          render(component, topic)
         }
 
         # TODO: Remove not_nil
-        component_connection.not_nil!.if_render_required(proc)
+        component_connections[topic].not_nil!.if_render_required(proc)
       end
     end
 
@@ -95,7 +98,7 @@ module Motion
       [identifier, data, command]
     end
 
-    private def render(component)
+    private def render(component, topic)
       html = Motion.html_transformer.add_state_to_html(component, component.rerender)
       rebroadcast!({
         subject: "message_new",
@@ -116,11 +119,11 @@ module Motion
     #   synchronize
     # end
 
-    private def process_periodic_timer
-      component_connection.not_nil!.periodic_timers.each do |timer|
+    private def process_periodic_timer(topic)
+      component_connections[topic].not_nil!.periodic_timers.each do |timer|
         name = timer[:name]
         self.fibers << spawn name: name.to_s do
-          while connected? && periodic_timer_active?(name)
+          while connected?(topic) && periodic_timer_active?(name)
             proc = ->do
               interval = timer[:interval]
               sleep interval if interval.is_a?(Time::Span)
@@ -129,16 +132,16 @@ module Motion
               method.call if method.is_a?(Proc(Nil))
             end
 
-            component_connection.not_nil!.process_periodic_timer(proc, name.to_s)
+            component_connections[topic].not_nil!.process_periodic_timer(proc, name.to_s)
 
-            synchronize(broadcast: true)
+            synchronize(topic: topic, broadcast: true)
           end
         end
       end
     end
 
-    private def connected?
-      !component_connection.nil?
+    private def connected?(topic)
+      !component_connections[topic]?.nil?
     end
 
     # TODO: Some way to allow users to invoke
