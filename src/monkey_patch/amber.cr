d1cdd99991659c4f5a7aa5c696a8f221b1894e5d
Andrew <awcrotwell@gmail.com>
2020-09-13 18:10:12 -0400

    Moved stream method to amber monkey_patch

--- src/monkey_patch/amber.cr
+++ src/monkey_patch/amber.cr
@@ -1,20 +1,24 @@
 # :nodoc:
 module Amber
   # :nodoc:
-  module Controller
-    # :nodoc:
-    module Helpers
-      # :nodoc:
-      module Render
-        include Motion::MountComponent
-        include Motion::HTML::SpecialtyTags
-        @[JSON::Field(ignore: true)]
-        getter view = IO::Memory.new
+  module Controller::Helpers::Render
+    include Motion::MountComponent
+    include Motion::HTML::SpecialtyTags
+    @[JSON::Field(ignore: true)]
+    getter view = IO::Memory.new
 
-        def render(component : Motion::Base.class)
-          m(component)
-          view.to_s
-        end
+    def render(component : Motion::Base.class)
+      m(component)
+      view.to_s
+    end
+  end
+
+  # :nodoc:
+  module WebSockets::ClientSocket
+    # :nodoc:
+    def self.broadcast(stream_topic : String)
+      if channel = get_topic_channel("motion")
+        channel.process_broadcast(stream_topic)
       end
     end
   end
